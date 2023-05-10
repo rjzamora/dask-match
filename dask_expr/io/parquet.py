d46e6de3842623583da61bf8294ddefdce9efab0
Rick Zamora <rzamora217@gmail.com>
2023-05-10 09:42:24 -0500

    simplify ReadParquet

--- dask_expr/io/parquet.py
+++ dask_expr/io/parquet.py
@@ -3,19 +3,8 @@ from __future__ import annotations
 import operator
 from functools import cached_property
 
-from dask.dataframe.io.parquet.core import (
-    ParquetFunctionWrapper,
-    get_engine,
-    process_statistics,
-    set_index_columns,
-)
-from dask.dataframe.io.parquet.utils import _split_user_options
-from dask.utils import natural_sort_key
-
 from dask_expr.expr import EQ, GE, GT, LE, LT, NE, Expr, Filter, Projection
 from dask_expr.io import BlockwiseIO, PartitionsFiltered
-
-NONE_LABEL = "__null_dask_index__"
 
 
 def _list_columns(columns):
@@ -68,19 +57,49 @@ class ReadParquet(PartitionsFiltered, BlockwiseIO):
         "_series": False,
     }
 
-    @property
-    def engine(self):
-        return get_engine("pyarrow")
+    @cached_property
+    def _ddf(self):
+        # Leverage dd.read_parquet for now to
+        # simplify development
+        import dask.dataframe as dd
+
+        return dd.read_parquet(
+            self.path,
+            columns=self.operand("columns"),
+            filters=self.filters,
+            categories=self.categories,
+            index=self.operand("index"),
+            storage_options=self.storage_options,
+            calculate_divisions=self.calculate_divisions,
+            ignore_metadata_file=self.ignore_metadata_file,
+            metadata_task_size=self.metadata_task_size,
+            split_row_groups=self.split_row_groups,
+            blocksize=self.blocksize,
+            aggregate_files=self.aggregate_files,
+            parquet_file_extension=self.parquet_file_extension,
+            filesystem=self.filesystem,
+            **self.kwargs,
+        )
 
     @property
-    def columns(self):
-        columns_operand = self.operand("columns")
-        if columns_operand is None:
-            return self._meta.columns
-        else:
-            import pandas as pd
+    def _meta(self):
+        meta = self._ddf._meta
+        if self._series:
+            column = _list_columns(self.operand("columns"))[0]
+            return meta[column]
+        return meta
 
-            return pd.Index(_list_columns(columns_operand))
+    def _divisions(self):
+        return self._ddf.divisions
+
+    @cached_property
+    def _tasks(self):
+        return list(self._ddf.dask.to_dict().values())
+
+    def _filtered_task(self, index: int):
+        if self._series:
+            return (operator.getitem, self._tasks[index], self.columns[0])
+        return self._tasks[index]
 
     def _simplify_up(self, parent):
         if isinstance(parent, Projection):
@@ -123,145 +142,3 @@ class ReadParquet(PartitionsFiltered, BlockwiseIO):
                     (column, op, value),
                 )
                 return ReadParquet(**kwargs)
-
-    @cached_property
-    def _dataset_info(self):
-        # Process and split user options
-        (
-            dataset_options,
-            read_options,
-            open_file_options,
-            other_options,
-        ) = _split_user_options(**(self.kwargs or {}))
-
-        # Extract global filesystem and paths
-        fs, paths, dataset_options, open_file_options = self.engine.extract_filesystem(
-            self.path,
-            self.filesystem,
-            dataset_options,
-            open_file_options,
-            self.storage_options,
-        )
-        read_options["open_file_options"] = open_file_options
-        paths = sorted(paths, key=natural_sort_key)  # numeric rather than glob ordering
-
-        auto_index_allowed = False
-        index_operand = self.operand("index")
-        if index_operand is None:
-            # User is allowing auto-detected index
-            auto_index_allowed = True
-        if index_operand and isinstance(index_operand, str):
-            index = [index_operand]
-        else:
-            index = index_operand
-
-        blocksize = self.blocksize
-        if self.split_row_groups in ("infer", "adaptive"):
-            # Using blocksize to plan partitioning
-            if self.blocksize == "default":
-                if hasattr(self.engine, "default_blocksize"):
-                    blocksize = self.engine.default_blocksize()
-                else:
-                    blocksize = "128MiB"
-        else:
-            # Not using blocksize - Set to `None`
-            blocksize = None
-
-        dataset_info = self.engine._collect_dataset_info(
-            paths,
-            fs,
-            self.categories,
-            index,
-            self.calculate_divisions,
-            self.filters,
-            self.split_row_groups,
-            blocksize,
-            self.aggregate_files,
-            self.ignore_metadata_file,
-            self.metadata_task_size,
-            self.parquet_file_extension,
-            {
-                "read": read_options,
-                "dataset": dataset_options,
-                **other_options,
-            },
-        )
-
-        # Infer meta, accounting for index and columns arguments.
-        meta = self.engine._create_dd_meta(dataset_info)
-        index = [index] if isinstance(index, str) else index
-        meta, index, columns = set_index_columns(
-            meta, index, self.operand("columns"), auto_index_allowed
-        )
-        if meta.index.name == NONE_LABEL:
-            meta.index.name = None
-        dataset_info["meta"] = meta
-        dataset_info["index"] = index
-        dataset_info["columns"] = columns
-
-        return dataset_info
-
-    @property
-    def _meta(self):
-        meta = self._dataset_info["meta"]
-        if self._series:
-            column = _list_columns(self.operand("columns"))[0]
-            return meta[column]
-        return meta
-
-    @cached_property
-    def _plan(self):
-        dataset_info = self._dataset_info
-        parts, stats, common_kwargs = self.engine._construct_collection_plan(
-            dataset_info
-        )
-
-        # Parse dataset statistics from metadata (if available)
-        parts, divisions, _ = process_statistics(
-            parts,
-            stats,
-            dataset_info["filters"],
-            dataset_info["index"],
-            (
-                dataset_info["blocksize"]
-                if dataset_info["split_row_groups"] is True
-                else None
-            ),
-            dataset_info["split_row_groups"],
-            dataset_info["fs"],
-            dataset_info["aggregation_depth"],
-        )
-
-        meta = dataset_info["meta"]
-        if len(divisions) < 2:
-            # empty dataframe - just use meta
-            divisions = (None, None)
-            io_func = lambda x: x
-            parts = [meta]
-        else:
-            # Use IO function wrapper
-            io_func = ParquetFunctionWrapper(
-                self.engine,
-                dataset_info["fs"],
-                meta,
-                dataset_info["columns"],
-                dataset_info["index"],
-                dataset_info["kwargs"]["dtype_backend"],
-                {},  # All kwargs should now be in `common_kwargs`
-                common_kwargs,
-            )
-
-        return {
-            "func": io_func,
-            "parts": parts,
-            "divisions": divisions,
-        }
-
-    def _divisions(self):
-        return self._plan["divisions"]
-
-    def _filtered_task(self, index: int):
-        tsk = (self._plan["func"], self._plan["parts"][index])
-        if self._series:
-            return (operator.getitem, tsk, self.columns[0])
-        return tsk
